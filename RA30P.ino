2dbaefd8902f15a5f9a3ba3c545285a1f548615f
Katherine <katherinemwen@gmail.com>
2019-06-28 13:45:45 -0400

    Change sensor pin numbers

--- RA30P.ino
+++ RA30P.ino
@@ -1,8 +1,8 @@
-int SensorPin = A0;   //analog pin 0
-int SensorPin1 = A1;  //analog pin 1
+int SensorPin1 = A1;   //analog pin 1
 int SensorPin2 = A2;  //analog pin 2
 int SensorPin3 = A3;  //analog pin 3
 int SensorPin4 = A4;  //analog pin 4
+int SensorPin5 = A5;  //analog pin 5
 
 int previous = 0; 
 int const threshold = 10;       //subject to change
@@ -12,32 +12,32 @@ void setup(){
 }
  
 void loop(){
-  int SensorReading = analogRead(SensorPin);          //Read pin 0
   int SensorReading1 = analogRead(SensorPin1);        //Read pin 1
   int SensorReading2 = analogRead(SensorPin2);        //Read pin 2
   int SensorReading3 = analogRead(SensorPin3);        //Read pin 3
   int SensorReading4 = analogRead(SensorPin4);        //Read pin 4
+  int SensorReading5 = analogRead(SensorPin5);        //Read pin 5
   
   int mfsr_r18 = map(SensorReading, 0, 1024);         //Mapping force values between 0 and 1024
   int asdf = map(SensorReading1, 0, 1024);            
 
-  if (sensorReading > 10) {   //keep
+  if (sensorReading > threshold) {   //keep
     
   }
 
-  if (sensorReading1 > 10) {
+  if (sensorReading1 > threshold) {
     //keep
   }
 
-  if (sensorReading2 > 10) {
+  if (sensorReading2 > threshold) {
     //keep
   }
 
-  if (sensorReading3 > 10) {
+  if (sensorReading3 > threshold) {
     //keep
   }
 
-  if (sensorReading4 > 10) {
+  if (sensorReading4 > threshold) {
     //keep
   }
 
@@ -48,7 +48,7 @@ void loop(){
   
   int difference = asdf - previous;
 //  if (difference > 20) {
-//    //Serial.println("Seond sensor being hit");
+//    //Serial.println("Second sensor being hit");
 //    //Serial.println(asdf);
 //    
 //  }
