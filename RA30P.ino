f76257170e6eae0af179a79b294fe93a7d119821
Katherine <katherinemwen@gmail.com>
2019-07-03 10:51:09 -0400

    Working code

--- RA30P.ino
+++ RA30P.ino
@@ -1,55 +1,123 @@
-int SensorPin1 = A1;  //analog pin 1 - Edge of drum
-int SensorPin2 = A2;  //analog pin 2 - Edge of drum
-int SensorPin3 = A3;  //analog pin 3 - Centre of drum
-int SensorPin4 = A4;  //analog pin 4 - Edge of drum
-int SensorPin5 = A5;  //analog pin 5 - Edge of drum
+#include <Wire.h> 
+#include <stdio.h>
 
-int sensorVelocity1 = 0;
-int sensorVelocity2 = 0;
-int sensorVelocity3 = 0;
-int sensorVelocity4 = 0;
-int sensorVelocity5 = 0;
+#define SensorPin1 A1 
+#define SensorPin2 A2
+#define SensorPin3 A3 
+#define SensorPin4 A4
+#define SensorPin5 A5 
 
-int const threshold = 10;  //subject to change
-int channel = 1;  // Defines the MIDI channel to send messages on
+const int threshold = 30;  //subject to change
+int channel = 9;  // Defines the MIDI channel to send messages on
+
+elapsedMillis timer = 0;
+
+int notes[] = {63,63,60,63,63};
+int prev_state[] = {0,0,0,0,0}; 
+int new_state[] = {0,0,0,0,0}; 
+
+//int prev_1 = 0;
+//int prev_2 = 0;
+//int prev_3 = 0;
+//int prev_4 = 0;
+//int prev_5 = 0;
+//
+//int new_1 = 0;
+//int new_2 = 0;
+//int new_3 = 0;
+//int new_4 = 0;
+//int new_5 = 0;
+
+
 
 void setup(){
   Serial.begin(9600);
 }
  
 void loop(){
+  getCurrentState();
 
-  // Filter out the forces below the threshold by setting the reading to 0
-  int sensorReading1 = analogRead(SensorPin1) >= threshold ? analogRead(SensorPin1) : 0;        //Read pin 1
-  int sensorReading2 = analogRead(SensorPin2) >= threshold ? analogRead(SensorPin2) : 0;        //Read pin 2
-  int sensorReading3 = analogRead(SensorPin3) >= threshold ? analogRead(SensorPin3) : 0;        //Read pin 3
-  int sensorReading4 = analogRead(SensorPin4) >= threshold ? analogRead(SensorPin4) : 0;        //Read pin 4
-  int sensorReading5 = analogRead(SensorPin5) >= threshold ? analogRead(SensorPin5) : 0;        //Read pin 5
+  if(timer > 180){
+    
+    // Check Pot
+    check_drums();
+
+    timer = 0;
+
+     for (int i = 0; i < 5; i++){
+    prev_state[i] = new_state[i];
+   }
+  }
   
-  calculateVelocities(sensorReading1, sensorReading2, sensorReading3, sensorReading4, sensorReading5);
-  sendNotes(sensorVelocity1, sensorVelocity2, sensorVelocity3, sensorVelocity4, sensorVelocity5);
+  // Update our state
+  //memcpy(new_state,prev_state, sizeof(prev_state));
+
+// for (int i = 0; i < 5; i++){
+//  prev_state[i] = new_state[i];
+// }
+    //prev_1 = new_1;
+    //new_1 = prev_1;
+    //Serial.println(new_1);
 }
 
-void calculateVelocities(int sensorReading1, int sensorReading2, int sensorReading3, int sensorReading4, int sensorReading5) {
-  // Calculate velocity for each sensor based on sensor reading - higher velocity -> louder, lower velocity -> quieter
-  // Sensor readings range from 0-1023 and velocity values range from 0-127
-  sensorVelocity1 = map(sensorReading1, 0, 1023, 0, 127);
-  sensorVelocity2 = map(sensorReading2, 0, 1023, 0, 127);
-  sensorVelocity3 = map(sensorReading3, 0, 1023, 0, 127);
-  sensorVelocity4 = map(sensorReading4, 0, 1023, 0, 127);
-  sensorVelocity5 = map(sensorReading5, 0, 1023, 0, 127);
+void getCurrentState() {
+  new_state[0] = analogRead(SensorPin1) >= threshold ? analogRead(SensorPin1) : 0;        //Read pin 1
+  new_state[1] = analogRead(SensorPin2) >= threshold ? analogRead(SensorPin2) : 0;        //Read pin 2
+  new_state[2] = analogRead(SensorPin3) >= threshold ? analogRead(SensorPin3) : 0;        //Read pin 3
+  new_state[3] = analogRead(SensorPin4) >= threshold ? analogRead(SensorPin4) : 0;        //Read pin 4
+  new_state[4] = analogRead(SensorPin5) >= threshold ? analogRead(SensorPin5) : 0;        //Read pin 5
+
+// new_1 = analogRead(SensorPin1) >= threshold ? analogRead(SensorPin1) : 0;  
+// new_2 = analogRead(SensorPin2) >= threshold ? analogRead(SensorPin2) : 0;  
+// new_3 = analogRead(SensorPin3) >= threshold ? analogRead(SensorPin3) : 0;  
+// new_4 = analogRead(SensorPin4) >= threshold ? analogRead(SensorPin4) : 0;  
+// new_5 = analogRead(SensorPin5) >= threshold ? analogRead(SensorPin5) : 0;  
+
+  Serial.print(new_state[0]); 
+  Serial.print(", "); 
+  Serial.print(new_state[1]); 
+  Serial.print(", "); 
+  Serial.print(new_state[2]); 
+  Serial.print(", "); 
+  Serial.print(new_state[3]); 
+  Serial.print(", "); 
+  Serial.print(new_state[4]); 
+  Serial.print('\n'); 
+
+//  FILE *out_file = fopen("user_test_session.txt", "w");
+//
+//  if (out_file == NULL) {
+//    printf("Error! Could not open file\n"); 
+//    exit(-1); // must include stdlib.h 
+//  }
+//
+//    fprintf(new_state, sizeof(int), (sizeof)new_state, out_file); // write to file
+//
+//  fclose(out_file);
+
+
+//    Serial.print("new: "); 
+//    Serial.println(new_state[0]);
+//    Serial.print("prev: "); 
+//    Serial.println(prev_state[0]);
+  
 }
 
-void sendNotes(int sensorVelocity1, int sensorVelocity2, int sensorVelocity3, int sensorVelocity4, int sensorVelocity5) {
-  // Send MIDI note based on sensor velocities
-  // Sensor 3 is A4 (note 69), all other sensors are F4 (note 64) - subject to change
-  // Send all notes from all sensors through MIDI - sequentially? 
-  
-  usbMIDI.sendNoteOn(69, sensorVelocity3, channel); // Turns note A4 on
-  usbMIDI.sendNoteOn(64, sensorVelocity1, channel); // Turns note F4 on
-  usbMIDI.sendNoteOn(64, sensorVelocity2, channel);
-  usbMIDI.sendNoteOn(64, sensorVelocity4, channel);
-  usbMIDI.sendNoteOn(64, sensorVelocity5, channel);
-  
-//  usbMIDI.sendNoteOff(69, velocity, channel);
+void check_drums(){
+  for(int n = 0; n < 5; n++){
+//     Serial.print("new: "); 
+//     Serial.println(new_state[n]);
+//     Serial.print("old: "); 
+//     Serial.println(prev_state[n]);
+     if((new_state[n] - prev_state[n] > 30) && (new_state[n] > 0)) {
+       int vel = map(new_state[n], 0, 1023, 0, 127); 
+       usbMIDI.sendNoteOn(notes[n], vel, channel);
+//       Serial.print("new: "); 
+//       Serial.println(new_state[n]);
+//       Serial.print("old: "); 
+//       Serial.println(prev_state[n]);
+     } else { 
+       usbMIDI.sendNoteOff(notes[n], 0, channel); 
+     }   
+  }
 }
