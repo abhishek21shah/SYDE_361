1d47a5f175d63ee4cf87ba652607c361f6a0afc5
Abhishek Shah <36013070+abhishek21shah@users.noreply.github.com>
2019-07-21 19:49:22 -0400

    Update Bongos_LatestBeta.ino

--- Bongos_LatestBeta.ino
+++ Bongos_LatestBeta.ino
@@ -1,45 +1,38 @@
 #include <Wire.h> 
 #include <stdio.h>
 
-#define SensorPin1 A1 
-#define SensorPin2 A2
-#define SensorPin3 A3 
-#define SensorPin4 A4
-#define SensorPin5 A5
-#define SensorPin6 A6
-#define SensorPin7 A7
-#define SensorPin8 A8
-#define SensorPin9 A9
-#define threshold 30      // Threshold to compare force values against
-#define channel 9         // Defines the MIDI channel to send messages on
+#define SensorPin1 A3
+#define SensorPin2 A8
+#define SensorPin3 A7
+#define SensorPin4 A1
+#define SensorPin5 A4
+#define SensorPin6 A9
+#define SensorPin7 A6
+#define SensorPin8 A2
+#define SensorPin9 A5
+
+const int threshold = 30;  //subject to change
+int channel = 9;  // Defines the MIDI channel to send messages on
 
 elapsedMillis timer = 0;
 
 int note;
 
-int number_of_sensors_triggered_Z1 = 0;
-int number_of_sensors_triggered_Z2 = 0;
-int number_of_sensors_triggered_Z3 = 0;
-
-int zone2[] = {0,0,0,0};
-int zone3[] = {0,0,0,0};
-
-int prev_state[] = {0,0,0,0,0,0,0,0,0};
+int prev_state[] = {0,0,0,0,0,0,0,0,0}; 
 int new_state[] = {0,0,0,0,0,0,0,0,0};
 
-void setup()
-{
+void setup(){
   Serial.begin(9600);
 }
  
 void loop(){
   getCurrentState();
 
-  if(timer > 220)
-  {
-    determineLocation();        // Check to see which zone(s) is/are being triggered
+  if(timer > 180){
 
-    check_drums();              // Check Pot
+    //determineLocation(); 
+    // Check Pot
+    check_drums();
 
     timer = 0;
 
@@ -50,66 +43,63 @@ void loop(){
   }
 }
 
-void determineLocation() 
-{
-  for (int x = 0; x < 4; x++) 
-  {
-    zone2[x] = new_state[x+3] - prev_state[x+3];
-    zone3[x] = new_state[x] - prev_state[x];
-  }
+void determineLocation() {
+  int number_of_sensors_triggered_Z1 = 0;
+  int number_of_sensors_triggered_Z2 = 0;
+  int number_of_sensors_triggered_Z3 = 0;
 
-  for (int i = 0; i < 4; i++) 
-  {
-    if (zone2[i+3] > threshold) 
-    { 
-       number_of_sensors_triggered_Z2 += 1; 
+  int zone2[] = {0,0,0,0};
+  int zone3[] = {0,0,0,0};
+  
+  for (int x = 0; x < 4; x++) {
+    zone2[x] = new_state[x+4];
+    zone3[x] = new_state[x];
+  }
+  
+  for (int i = 0; i < 4; i++) {
+    if (zone2[i] > threshold) { 
+       number_of_sensors_triggered_Z2+=1; 
     }
     
-    if (zone3[i] > threshold) 
-    { 
-       number_of_sensors_triggered_Z3 += 1; 
+    if (zone3[i] > threshold) { 
+       number_of_sensors_triggered_Z3+=1; 
     }
   } 
 
-  if ((new_state[8] - prev_state[8]) > threshold) 
+  Serial.println(number_of_sensors_triggered_Z2);
+  
+  if ((new_state[8]) > threshold) 
   { 
     number_of_sensors_triggered_Z1 = 1; 
   }
     
-  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 > 0) && (number_of_sensors_triggered_Z1 > 0))         // All 3 zones are triggered
-  {
-    note = 39; 
+  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 > 0) && (number_of_sensors_triggered_Z1 > 0)) { // All 3 are triggered
+    note = 43;
   }
 
-  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 > 0) && (number_of_sensors_triggered_Z1 == 0))        // Zones 1 & 3 are triggered
-  { 
-    note = 38; 
+  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 > 0) && (number_of_sensors_triggered_Z1 == 0)) { // z2 and z3 are triggered
+    note = 39;
   }
 
-  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 == 0) && (number_of_sensors_triggered_Z1 > 0))        // Zones 1 & 2 are triggered
-  { 
-    note = 40; 
+  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 == 0) && (number_of_sensors_triggered_Z1 > 0)) { // z2 and z1 are triggered
+    note = 41;
   }
 
-  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 == 0) && (number_of_sensors_triggered_Z1 == 0))       // ONLY Zone 2 is triggered
-  { 
-    note = 42; 
-  }
-
-  if ((number_of_sensors_triggered_Z2 == 0) && (number_of_sensors_triggered_Z3 > 0) && (number_of_sensors_triggered_Z1 == 0))       // ONLY Zone 3 is triggered
-  {         
+  if ((number_of_sensors_triggered_Z2 > 0) && (number_of_sensors_triggered_Z3 == 0) && (number_of_sensors_triggered_Z1 == 0)) { // ONLY z2 is triggered
     note = 40;
   }
 
-  if ((number_of_sensors_triggered_Z2 == 0) && (number_of_sensors_triggered_Z3 == 0) && (number_of_sensors_triggered_Z1 > 0))       // ONLY Zone 1 is triggered
-  { 
-    note = 38; 
+  if ((number_of_sensors_triggered_Z2 == 0) && (number_of_sensors_triggered_Z3 > 0) && (number_of_sensors_triggered_Z1 == 0)) { // ONLY z3 is triggered
+    note = 38;
   }
 
+  if ((number_of_sensors_triggered_Z2 == 0) && (number_of_sensors_triggered_Z3 == 0) && (number_of_sensors_triggered_Z1 > 0)) { // ONLY z1 is triggered
+    note = 42;
+  }
+  
 }
 
-void getCurrentState() 
-{
+void getCurrentState() {
   new_state[0] = analogRead(SensorPin1) >= threshold ? analogRead(SensorPin1) : 0;        //Read pin 1
   new_state[1] = analogRead(SensorPin2) >= threshold ? analogRead(SensorPin2) : 0;        //Read pin 2
   new_state[2] = analogRead(SensorPin3) >= threshold ? analogRead(SensorPin3) : 0;        //Read pin 3
@@ -119,6 +109,8 @@ void getCurrentState()
   new_state[6] = analogRead(SensorPin7) >= threshold ? analogRead(SensorPin7) : 0;        //Read pin 7
   new_state[7] = analogRead(SensorPin8) >= threshold ? analogRead(SensorPin8) : 0;        //Read pin 8
   new_state[8] = analogRead(SensorPin9) >= threshold ? analogRead(SensorPin9) : 0;        //Read pin 9
+
+  determineLocation(); 
   
   Serial.print(new_state[0]); 
   Serial.print(", "); 
@@ -137,20 +129,15 @@ void getCurrentState()
   Serial.print(new_state[7]);
   Serial.print(", "); 
   Serial.print(new_state[8]);
-  Serial.println();
+  Serial.print('\n');
 }
 
-void check_drums()
-{
-  for (int n = 0; n < 9; n++)
-  {
-     if ((new_state[n] - prev_state[n] > threshold) && (new_state[n] > 0)) 
-     {
+void check_drums(){
+  for(int n = 0; n < 9; n++){
+     if((new_state[n] - prev_state[n] > threshold) && (new_state[n] > 0)) {
        int vel = map(new_state[n], 0, 1023, 0, 127); 
        usbMIDI.sendNoteOn(note, vel, channel);
-     } 
-     else 
-     { 
+     } else { 
        usbMIDI.sendNoteOff(note, 0, channel); 
      }   
   }
